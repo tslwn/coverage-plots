4fc3ea060975f8f9d22ac467ffc5a29c67f28ee3
Tim Lawson <qr23940@bristol.ac.uk>
2023-10-01 15:55:51 +0100

    Add class ratios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,6 +49,10 @@ function isValidValue(value: string) {
   return parseFloat(value) >= 0 && parseFloat(value) <= 1;
 }
 
+function capitalize(value: string) {
+  return value[0].toUpperCase() + value.slice(1);
+}
+
 export default function Home() {
   // TODO: show derivative measures
   const [cellData, setCellData] = React.useState<Record<DataKey, string>>({
@@ -99,27 +103,6 @@ export default function Home() {
     }));
   };
 
-  const handleConfirm = (
-    value: string,
-    rowIndex: number | undefined,
-    columnIndex: number | undefined
-  ) => {
-    if (rowIndex == undefined || columnIndex == undefined) {
-      return;
-    }
-    const dataKey = getDataKey(rowIndex, columnIndex);
-    setCellData((prev) => ({
-      ...prev,
-      [dataKey]: value,
-    }));
-    if (columnIndex !== 0) {
-      setCellIntent((prev) => ({
-        ...prev,
-        [dataKey]: isValidValue(value) ? null : Intent.DANGER,
-      }));
-    }
-  };
-
   const cellRenderer = (rowIndex: number, columnIndex: number) => {
     const dataKey = getDataKey(rowIndex, columnIndex);
     const value = cellData[dataKey];
@@ -131,13 +114,15 @@ export default function Home() {
         intent={cellIntent[dataKey]}
         onCancel={handleValidate}
         onChange={handleValidate}
-        onConfirm={handleConfirm}
+        onConfirm={handleValidate}
       />
     );
   };
 
   const scatterData = getScatterData(cellData);
-  const dominated = getDominatedPairs(scatterData);
+  const dominatedPairs = getDominatedPairs(scatterData);
+  const convexHull = getConvexHull(scatterData);
+  const preferredPairs = getPreferredPairs(getConvexHull(scatterData));
 
   return (
     <div className="p-8">
@@ -161,12 +146,22 @@ export default function Home() {
             </Table2>
           </div>
           <div>
-            {/* TODO: show class ratios for which models are more accurate */}
             <h2 className="mb-4 text-xl">Which models are dominated?</h2>
-            <ul className="list-disc list-inside">
-              {dominated.map(([datum1, datum2], index) => (
+            <ul className="list-disc list-inside mb-4">
+              {dominatedPairs.map(([datum1, datum2], index) => (
                 <li key={index}>
                   {datum2.model} is dominated by {datum1.model}
+                </li>
+              ))}
+            </ul>
+            <h2 className="mb-4 text-xl">
+              When do models have greater accuracy?
+            </h2>
+            <ul className="list-disc list-inside">
+              {preferredPairs.map(([datum1, datum2, slope], index) => (
+                <li key={index}>
+                  {capitalize(datum2.model)} is preferred to {datum1.model} when
+                  the class ratio ≥ {slope.toLocaleString()}
                 </li>
               ))}
             </ul>
@@ -191,7 +186,7 @@ export default function Home() {
               dependentAxis
             />
             <VictoryLine
-              data={getConvexHull(scatterData)}
+              data={convexHull}
               labels={({ datum }: { datum: Datum }) =>
                 datum.x === 1 ? "Convex hull" : null
               }
@@ -242,10 +237,10 @@ function getScatterData(data: Record<DataKey, string>): Datum[] {
   return result;
 }
 
-function pairs(data: Datum[]): [Datum, Datum][] {
+function pairs(scatterData: Datum[]): [Datum, Datum][] {
   const result: [Datum, Datum][] = [];
-  for (const datum1 of data) {
-    for (const datum2 of data) {
+  for (const datum1 of scatterData) {
+    for (const datum2 of scatterData) {
       if (datum1.model !== datum2.model) {
         result.push([datum1, datum2]);
       }
@@ -258,20 +253,34 @@ function isDominated(datum1: Datum, datum2: Datum): boolean {
   return datum1.x <= datum2.x && datum1.y >= datum2.y;
 }
 
-function getDominatedPairs(data: Datum[]): [Datum, Datum][] {
-  return pairs(data)
+function getDominatedPairs(scatterData: Datum[]): [Datum, Datum][] {
+  return pairs(scatterData)
     .filter(([datum1, datum2]) => isDominated(datum1, datum2))
     .sort(([, datum1], [, datum2]) => datum1.model.localeCompare(datum2.model));
 }
 
-function getConvexHull(data: Datum[]): Datum[] {
-  const dominatedPairs = getDominatedPairs(data);
+function getConvexHull(scatterData: Datum[]): Datum[] {
+  const dominatedPairs = getDominatedPairs(scatterData);
   return [
-    { model: "Always positive", x: 1, y: 1 },
-    { model: "Always negative", x: 0, y: 0 },
-  ].concat(
-    data.filter(
-      (datum) => !dominatedPairs.some(([_, datum2]) => datum === datum2)
+    { model: "always positive", x: 1, y: 1 },
+    { model: "always negative", x: 0, y: 0 },
+  ]
+    .concat(
+      scatterData.filter(
+        (datum) => !dominatedPairs.some(([_, datum2]) => datum === datum2)
+      )
     )
-  );
+    .sort((a, b) => a.x - b.x);
+}
+
+// This depends on `getConvexHull` returning the points in order of increasing x.
+function getPreferredPairs(convexHull: Datum[]): [Datum, Datum, number][] {
+  const results: [Datum, Datum, number][] = [];
+  for (let index = 0; index < convexHull.length - 1; index++) {
+    const datum1 = convexHull[index];
+    const datum2 = convexHull[index + 1];
+    const slope = (datum2.y - datum1.y) / (datum2.x - datum1.x);
+    results.push([datum1, datum2, slope]);
+  }
+  return results;
 }
